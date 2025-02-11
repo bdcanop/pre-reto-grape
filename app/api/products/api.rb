fd0912a115c84ea0e06e3c6a06c318bbabe889d9
Daniel <bdcanop@eafit.edu.co>
2025-02-10 21:02:54 -0500

    Algunos tests agregados

--- app/api/products/api.rb
+++ app/api/products/api.rb
@@ -1,45 +1,41 @@
-module Products 
+module Products
     class API < Grape::API
-        version 'v1'
+        version "v1"
         format :json
         prefix :api
 
         resource :products do
-        # Obtener todos los productos
             get do
                 Product.all
             end
-        
-            # Obtener un producto por ID
-            get ':id' do
+
+            get ":id" do
                 Product.find(params[:id])
             end
-        
-            # Crear un nuevo producto
+
             params do
-                requires :name, type: String, desc: "Nombre del producto"
-                requires :price, type: BigDecimal, desc: "Precio del producto"
-                requires :stock, type: Integer, desc: "Cantidad del producto"
+                requires :name, type: String, desc: "Product name"
+                requires :price, type: BigDecimal, desc: "Product price"
+                requires :stock, type: Integer, desc: "Quantity of stock"
             end
             post do
                 Product.create!(declared(params))
             end
-        
-            # Actualizar un producto
+
             params do
-                optional :name, type: String, desc: "Nuevo nombre del producto"
-                optional :price, type: BigDecimal, desc: "Nuevo precio del producto"
-                optional :stock, type: Integer, desc: "Nueva cantidad del producto"
+                optional :name, type: String, desc: "New Product name"
+                optional :price, type: BigDecimal, desc: "New Product price"
+                optional :stock, type: Integer, desc: "New Quantity of stock"
             end
-            put ':id' do
+            put ":id" do
                 product = Product.find(params[:id])
                 product.update!(declared(params, include_missing: false))
                 product
             end
-        
-            # Eliminar un producto
-            delete ':id' do
+
+            delete ":id" do
                 Product.find(params[:id]).destroy
+                status 204
             end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount Products::API => '/'
+  mount Products::API => "/"
 end
